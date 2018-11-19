15b68abb3b52fe8f9ba769e7c84c45245517ca13
jan2nov <jnovotny@arcus-htc-gpu008.arcus-htc.arc.local>
2018-11-19 17:34:06 +0000

    adding more variablity in 1D FFT to the script call

--- script/call.sh
+++ script/call.sh
@@ -4,18 +4,20 @@
 #where the memory_file is a list of permited memory settings on the gpu
 
 echo "Starting"
-rm timing.txt
 # setup your id of the card and the base of memory frequency
-CARD=1
-FREQ_MEM=4513
+CARD=0
+FREQ_MEM=3003
+LENGTH=128
+
+rm timing-P4-${LENGTH}-0-0.txt
 
 #run the logger on the energy profiling
-	nvidia-smi --query-gpu=timestamp,power.draw,clocks.current.sm,clocks.current.memory --format=csv,noheader,nounits -i $CARD -f huh.txt -lms 10 &
+	nvidia-smi --query-gpu=timestamp,power.draw,clocks.current.sm,clocks.current.memory --format=csv,noheader,nounits -i $CARD -f nvidiasmi-P4-${LENGTH}-0-0.txt -lms 10 &
 	PID=$(echo $!)
 	echo "Logging process id: $PID"
 	sleep 5
 
-	
+echo "Running the ${LENGTH}"	
 #run for each permitted memory a defined cuFFT
 #need to change the length of cuFFT, now is setup to run on 2GB of data, i.e. number of FFT auto computed to fit 2GB with the set length
 while IFS='' read -r line || [[ -n "$line" ]]; do
@@ -24,7 +26,7 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	echo $CORE_MEM
 	nvidia-smi -i $CARD -ac $FREQ_MEM,$CORE_MEM
 	TIMESTAMP=$(date +"%Y/%m/%d %H:%M:%S.%3N")
-	nvprof --print-gpu-trace -u ms --csv ../cuFFT_benchmark.exe 2097152 0 0 -2 200 f R2C $CARD 2>out.nvprof
+	nvprof --print-gpu-trace -u ms --csv ../cuFFT_benchmark.exe ${LENGTH} 0 0 -2 200 f R2C $CARD 2>out.nvprof
 	START_LINE=$[$(awk '/Start/{ print NR; exit }' out.nvprof) + 3]
 	#in csv mode there is no line with Regs
 	#	END_LINE=$[$(awk '/Regs:/{ print NR; exit }' out.nvprof) - 3]
@@ -33,7 +35,7 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	END_TIME=$(head -n $END_LINE out.nvprof | tail -n 1 | awk -F "," '{print $1}')
 	END_PLUS_TIME=$(head -n $END_LINE out.nvprof | tail -n 1 | awk -F "," '{print $2}')
 	sleep 5
-	echo ${TIMESTAMP}"," ${START_TIME}"," ${END_TIME}"," ${END_PLUS_TIME} >> timing.txt
+	echo ${TIMESTAMP}"," ${START_TIME}"," ${END_TIME}"," ${END_PLUS_TIME}"," ${CORE_MEM}>> timing-P4-${LENGTH}-0-0.txt
 done < "$1"
 
 # clean-up 
