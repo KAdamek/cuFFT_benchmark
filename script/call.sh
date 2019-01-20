a84fe548eee595074ac8bad58363c603bed7f8db
Jan Novotny <jan.novotny@fpf.slu.cz>
2019-01-20 16:08:44 +0000

    update of the call.sh for 1d, reflects now the precision

--- script/call.sh
+++ script/call.sh
@@ -10,13 +10,16 @@ echo "Starting"
 # setup your id of the card and the base of memory frequency
 CARD=0
 FREQ_MEM=3003
+TYPE=C2C
+ID=P4
+PREC=d
 
-for LENGTH in 128 #32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 #2097152
+for LENGTH in 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152
 do
-rm timing-P4-${LENGTH}-0-0.txt
+rm timing-${ID}-${PREC}-${TYPE}-${LENGTH}-0-0.txt
 
 #run the logger on the energy profiling
-	nvidia-smi --query-gpu=timestamp,power.draw,clocks.current.sm,clocks.current.memory --format=csv,noheader,nounits -i $CARD -f nvidiasmi-P4-${LENGTH}-0-0.txt -lms 10 &
+	nvidia-smi --query-gpu=timestamp,power.draw,clocks.current.sm,clocks.current.memory --format=csv,noheader,nounits -i $CARD -f nvidiasmi-${ID}-${PREC}-${TYPE}-${LENGTH}-0-0.txt -lms 10 &
 	PID=$(echo $!)
 	echo "Logging process id: $PID"
 	sleep 5
@@ -30,7 +33,7 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	echo $CORE_MEM
 	nvidia-smi -i $CARD -ac $FREQ_MEM,$CORE_MEM
 	TIMESTAMP=$(date +"%Y/%m/%d %H:%M:%S.%3N")
-	nvprof --print-gpu-trace -u ms --csv ../cuFFT_benchmark.exe ${LENGTH} 0 0 -2 200 f C2R $CARD 2>out.nvprof
+	nvprof --print-gpu-trace -u ms --csv ../cuFFT_benchmark.exe ${LENGTH} 0 0 -2 200 ${PREC} ${TYPE} ${CARD} 2>out.nvprof
 	START_LINE=$[$(awk '/Start/{ print NR; exit }' out.nvprof) + 3]
 	#in csv mode there is no line with Regs
 	#	END_LINE=$[$(awk '/Regs:/{ print NR; exit }' out.nvprof) - 3]
@@ -39,7 +42,7 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	END_TIME=$(head -n $END_LINE out.nvprof | tail -n 1 | awk -F "," '{print $1}')
 	END_PLUS_TIME=$(head -n $END_LINE out.nvprof | tail -n 1 | awk -F "," '{print $2}')
 	sleep 5
-	echo ${TIMESTAMP}"," ${START_TIME}"," ${END_TIME}"," ${END_PLUS_TIME}"," ${CORE_MEM}>> timing-P4-${LENGTH}-0-0.txt
+	echo ${TIMESTAMP}"," ${START_TIME}"," ${END_TIME}"," ${END_PLUS_TIME}"," ${CORE_MEM}>> timing-${ID}-${PREC}-${TYPE}-${LENGTH}-0-0.txt
 done < "$1"
 
 # clean-up 
